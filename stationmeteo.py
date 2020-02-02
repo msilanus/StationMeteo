a34787745973589cda99e806e46a56ef654f57c8
Marc Silanus <msilanus@gmail.com>
2020-02-02 22:28:20 +0100

    First commit

---
+++ stationmeteo.py
@@ -0,0 +1,9 @@
+import urllib3
+import json
+
+http = urllib3.PoolManager()
+url_param="https://api.openweathermap.org/data/2.5/weather?q=Le+Thor,fr&appid=7585221fbcead099c4e4c4bb6fd3b68f"
+r = http.request('GET',url_param)
+reponse=json.loads(r.data.decode('utf-8'))
+print(reponse)
+print(type(reponse))
